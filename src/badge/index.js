2910829b55f9375822e7eea35ed05931e7752cfa
周迪 <772345373@qq.com>
2019-08-27 17:39:28 +0800

    feat: 添加badge

---
+++ src/badge/index.js
@@ -0,0 +1,21 @@
+Component({
+  externalClasses: ['my-badge'],
+  properties: {
+    count: {
+      type: Number,
+      value: 199,
+    },
+    over: {
+      type: Number,
+      value: 99,
+    },
+    dot: {
+      type: Boolean,
+      value: false,
+    },
+    disable: {
+      type: Boolean,
+      value: false,
+    },
+  },
+})
